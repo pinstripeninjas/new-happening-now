b3e4630a62c45df29bc33074724eb3d520b55ab0
pinstripeninjas <pinstripeninjas@gmail.com>
2020-08-07 21:25:01 -0700

    leaflet added with nowcoast mrms data

--- src/components/Graphic.jsx
+++ src/components/Graphic.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Box, Heading, Flex, Divider, Text, Stack, Icon } from "@chakra-ui/core";
+import { Map, TileLayer, WMSTileLayer } from "react-leaflet";
+import { connect } from "react-redux";
 import StormMotion from "./StormMotion";
 
-const Graphic = ({ options }) => {
-	const fillActions = () => {
-		return options.map((action, i) => {
+class Graphic extends React.Component {
+	state = {
+		center: [32.2, -111.0],
+		zoom: 8,
+	};
+
+	fillActions = () => {
+		return this.props.options.map((action, i) => {
 			return action.isChecked ? (
 				<Stack key={i} isInline spacing={2} align="center">
 					<Icon name="warning" size="20px" color="red.500" />
@@ -14,25 +21,39 @@ const Graphic = ({ options }) => {
 		});
 	};
 
-	return (
-		<Box border="1px" rounded="md" borderColor="gray.200" boxShadow="md" w={1100} h={550}>
-			<Flex h="100%">
-				<Box w={450} h="100%" position="relative" p={5}>
-					<Heading textAlign="center">Happening Now</Heading>
-					<Divider />
-					<Stack>{fillActions()}</Stack>
-					<StormMotion />
-				</Box>
-				<Box
-					as="iframe"
-					w="100%"
-					h="100%"
-					// src="https://noaa.maps.arcgis.com/apps/Minimalist/index.html?appid=c6b1683232fe4bb9a10833c6dffbcd04"
-					alt="demo"
-				/>
-			</Flex>
-		</Box>
-	);
-};
+	render() {
+		return (
+			<Box border="1px" rounded="md" borderColor="gray.200" boxShadow="md" w={1100} h={550}>
+				<Flex h="100%">
+					<Box w={450} h="100%" position="relative" p={5}>
+						<Heading textAlign="center">Happening Now</Heading>
+						<Divider />
+						<Stack>{this.fillActions()}</Stack>
+						<StormMotion />
+					</Box>
+					<Box h="100%" w="100%">
+						<Map center={this.state.center} zoom={this.state.zoom}>
+							<TileLayer
+								attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+								url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+							/>
+							<WMSTileLayer
+								url="https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer?"
+								layers="1"
+								format="image/png"
+								transparent="true"
+								opacity=".5"
+							/>
+						</Map>
+					</Box>
+				</Flex>
+			</Box>
+		);
+	}
+}
 
-export default Graphic;
+const mapStateToProps = (state) => ({
+	options: state.options,
+});
+
+export default connect(mapStateToProps)(Graphic);
