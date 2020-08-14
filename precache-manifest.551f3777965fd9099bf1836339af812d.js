257c825aaffe16ebbe89577233c3c60512074d7b
pinstripeninjas <pinstripeninjas@gmail.com>
2020-08-14 12:09:14 -0700

    Updates

---
+++ precache-manifest.551f3777965fd9099bf1836339af812d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311d22e0a2f9a17a62f267417a28d584",
+    "url": "/new-happening-now/index.html"
+  },
+  {
+    "revision": "bc2732edb847c3e0e283",
+    "url": "/new-happening-now/static/css/main.03a0fd74.chunk.css"
+  },
+  {
+    "revision": "e8421fa30236e60aa415",
+    "url": "/new-happening-now/static/js/2.8ecd6aa8.chunk.js"
+  },
+  {
+    "revision": "82528e3b7333d9020491ab508f9a9c16",
+    "url": "/new-happening-now/static/js/2.8ecd6aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2732edb847c3e0e283",
+    "url": "/new-happening-now/static/js/main.8920d641.chunk.js"
+  },
+  {
+    "revision": "997896ac55277a2c0d9b",
+    "url": "/new-happening-now/static/js/runtime-main.78370d51.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/new-happening-now/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/new-happening-now/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/new-happening-now/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/new-happening-now/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/new-happening-now/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
